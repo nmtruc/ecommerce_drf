dcabbb2beb1821194eb5a2bac082626466107ded
truc-nguyenminh <truc.nguyenminh93@gmail.com>
2022-03-06 17:32:33 +0700

    register user

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -5,6 +6,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import User, Product
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from django.contrib.auth.hashers import make_password
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -20,13 +22,21 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-@api_view(['GET'])
-def get_routes(request):
-    routes = [
-        '/api/products/',
-        '/api/product/<id>',
-    ]
-    return Response(routes)
+@api_view(['POST'])
+def register_user(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password']),
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
