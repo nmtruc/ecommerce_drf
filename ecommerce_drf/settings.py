89c782c7b8c11e7bb4209940f2ba6332145c2181
truc-nguyenminh <truc.nguyenminh93@gmail.com>
2022-03-07 21:47:19 +0700

    backend finished

--- ecommerce_drf/settings.py
+++ ecommerce_drf/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,7 +98,9 @@ ROOT_URLCONF = 'ecommerce_drf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -163,6 +166,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'frontend/build/static'
+]
 
 MEDIA_URL = 'images/'
 
--- ecommerce_drf/urls.py
+++ ecommerce_drf/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('api/users/', include('backend.urls.user_urls')),
     path('api/products/', include('backend.urls.product_urls')),
     path('api/orders/', include('backend.urls.order_urls')),
